f8a5f6ffed105b6dd784792809bd390a91b6ccb7
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-05 21:58:09 +0200

    [FIX]Navbar-logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,25 @@
-import logo from "../assets/img/dc-logo.png"
-function Navbar(){
-    return(
-        
-        <div className="container">
-        <nav className="navbar">
-          <div className="logo"><img src={logo} alt="DC Logo" />
-          </div>
-          <ul className="nav-links">
-            <li>CHARACTERS</li>
-            <li>COMICS</li>
-            <li>MOVIES</li>
-            <li>TV</li>
-            <li>GAMES</li>
-            <li>COLLECTIBLES</li>
-            <li>VIDEOS</li>
-            <li>FANS</li>
-            <li>NEWS</li>
-            <li>SHOP</li>
-          </ul>
-        </nav>
-        </div>
-        
-      );
+import logo from "../assets/img/dc-logo.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={logo} alt="DC Logo" />
+      </div>
+      <ul className="nav-links">
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
